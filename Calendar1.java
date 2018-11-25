ad885127c6b8dbf24bc2d3732be791eb10303f4f
HItomi <16ec048@ms.dendai.ac.jp>
2018-11-25 12:35:16 +0900

    色変更の追加

--- Calendar1.java
+++ Calendar1.java
@@ -5,26 +5,33 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Calendar1 extends Application {
     
     Stage stage;
+    Scene scene;
     LocalDate d;
     String[] week = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
     Label lbl;
-    Button btnpre, btnnext;
-    int year, month, start, day, i=0;
+    Button btnpre, btnnext,btnok;
+    int year, month, start, length, day, i=0,count;
+    //色変え
+    ComboBox<String> changeday;
+    String[] colors ={"RED","BLUE","BLACK"};
+    ComboBox<String> color;
+    
     Pane sp1 = new Pane(),sp2 = new Pane();
-    HBox h1;
     GridPane grid = new GridPane();
-    Scene scene;
+    HBox h1,h2;
     
     @Override
     public void start(Stage stage) {
@@ -69,8 +76,7 @@ public class Calendar1 extends Application {
         i=0;
         for(int j=0;j<7;j++){
             Label lbday = new Label(week[j]);
-            GridPane.setConstraints(lbday,j,i);
-            grid.getChildren().add(lbday);
+            grid.add(lbday,j,i);
         }
         //月初めの曜日を取得
         DayOfWeek firstday = d.withDayOfMonth(1).getDayOfWeek();
@@ -91,22 +97,20 @@ public class Calendar1 extends Application {
                 start = 6; break;
         }
         //その月の日数を取得
-        int length = d.lengthOfMonth();
+        length = d.lengthOfMonth();
         //日にちを入れる
         i=1;
         day=0;
         for(int j=start;j<7;j++){
             Label lbday = new Label(String.valueOf(day+1));
-            GridPane.setConstraints(lbday,j,i);
-            grid.getChildren().add(lbday);
+            grid.add(lbday,j,i);
             day++;
         }
         i++;
         while(day<length){
             for(int j=0;j<7;j++){
                 Label lbday = new Label(String.valueOf(day+1));
-                GridPane.setConstraints(lbday,j,i);
-                grid.getChildren().add(lbday);
+                grid.add(lbday,j,i);
                 day++;
                 if(day>=length) break;
             }
@@ -115,14 +119,76 @@ public class Calendar1 extends Application {
     }
     
     void Set(){
-        //ヘッダー部
+        //上部
         h1 = new HBox(30,btnpre,sp1,lbl,sp2,btnnext);
         HBox.setHgrow(sp1,Priority.ALWAYS);
         HBox.setHgrow(sp2,Priority.ALWAYS);
+        //下部
+        //日付・色選択
+        changeday = new ComboBox<>();
+        for(int n=1;n<length+1;n++){
+            changeday.getItems().add(String.valueOf(n));
+        }
+        color = new ComboBox<>();
+        color.getItems().addAll(colors);
+        btnok = new Button("OK");
+        btnok.setOnAction(event ->{
+            int change = Integer.parseInt(changeday.getValue());
+            ChangeColor(change,color.getValue());
+        });
+        h2 = new HBox(30,changeday,color,btnok);
         //配置
-        VBox v1 = new VBox(30,h1,grid);
+        VBox v1 = new VBox(30,h1,grid,h2);
         VBox.setMargin(h1,new Insets(20));
-        scene = new Scene(v1, 400, 400);
+        VBox.setMargin(h2,new Insets(20));
+        scene = new Scene(v1);
+    }
+    
+    //文字色変更
+    void ChangeColor(int cday, String color){
+        int x,y;//指定の日にちの座標
+        if(cday<=(7-start)){
+            x = start+cday-1;
+            y = 1;
+        } else{
+            x = (cday-start)%7;
+            y = ((cday-start)/7)+2;
+        }
+        switch(color){
+            case "RED":
+                for(int k=7;k<length;k++){
+                    if(grid.getChildren().get(k).toString()==String.valueOf(cday)){
+                        grid.getChildren().remove(k);
+                        break;
+                    }
+                }
+                Label clbl1 = new Label(String.valueOf(cday));
+                clbl1.setTextFill(Color.RED);
+                grid.add(clbl1,x,y);
+                break;
+            case "BLUE":
+                for(int k=7;k<length;k++){
+                    if(grid.getChildren().get(k).toString()==String.valueOf(cday)){
+                        grid.getChildren().remove(k);
+                        break;
+                    }
+                }
+                Label clbl2 = new Label(String.valueOf(cday));
+                clbl2.setTextFill(Color.BLUE);
+                grid.add(clbl2,x,y);
+                break;
+            case "BLACK":
+                for(int k=7;k<length;k++){
+                    if(grid.getChildren().get(k).toString()==String.valueOf(cday)){
+                        grid.getChildren().remove(k);
+                        break;
+                    }
+                }
+                Label clbl3 = new Label(String.valueOf(cday));
+                clbl3.setTextFill(Color.BLACK);
+                grid.add(clbl3,x,y);
+                break;
+        }
     }
     
     public static void main(String[] args) {
